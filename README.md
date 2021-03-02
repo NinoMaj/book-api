3b887b12b76cc6d30f167bc77f85a0c48c6b262b
Nino Majder <nino.majder@gmail.com>
2021-03-02 22:59:47 +0100

    Init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+### Simple web books api made in Rust
+
+To run api:
+
+1. Create `.env` file
+1. Add `DATABASE_URL` string in it?
+1. Cargo run
---
+++ src/main.rs
@@ -0,0 +1,33 @@
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
+
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+use dotenv::dotenv;
+use std::env;
+
+pub mod models;
+pub mod schema;
+
+fn main() {
+    // embedded_migrations::run(&db_conn)
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let conn = PgConnection::establish(&database_url)
+        .expect(&format!("Error connecting to {}", database_url));
+
+    let book = models::NewBook {
+        title: String::from("Gravity's Rainbow"),
+        author: String::from("Thomas Pynchon"),
+        published: true,
+    };
+
+    if models::Book::insert(book, &conn) {
+        println!("success");
+    } else {
+        println!("failed");
+    }
+}
