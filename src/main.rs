38eb63c522ca30afd7986dcd6c047a91c2aa4385
Nino Majder <nino.majder@gmail.com>
2021-03-28 14:18:12 +0200

    Add all routes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+#![feature(
+    plugin,
+    custom_derive,
+    const_fn,
+    decl_macro,
+    custom_attribute,
+    proc_macro_hygiene
+)]
+#![allow(proc_macro_derive_resolution_fallback, unused_attributes)]
+
+#[macro_use]
+extern crate diesel;
+
+extern crate dotenv;
+extern crate r2d2;
+extern crate r2d2_diesel;
+#[macro_use]
+extern crate rocket;
+extern crate rocket_contrib;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate serde_json;
+
 #[macro_use]
 extern crate diesel;
 extern crate dotenv;
@@ -7,27 +31,25 @@ use diesel::prelude::*;
 use dotenv::dotenv;
 use std::env;
 
+pub mod db;
 pub mod models;
 pub mod schema;
 
-fn main() {
-    // embedded_migrations::run(&db_conn)
+fn rocket() -> rocket::Rocket {
     dotenv().ok();
 
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let database_url = env::var("DATABASE_URL").expect("set DATABASE_URL");
 
-    let conn = PgConnection::establish(&database_url)
-        .expect(&format!("Error connecting to {}", database_url));
+    let pool = db::init_pool(database_url);
+    rocket::ignite()
+        .manage(pool)
+        .mount(
+            "/api/v1/",
+            routes![index, new, show, delete, author, update],
+        )
+        .mount("/", routes![static_files::all, static_files::index])
+}
 
-    let book = models::NewBook {
-        title: String::from("Gravity's Rainbow"),
-        author: String::from("Thomas Pynchon"),
-        published: true,
-    };
-
-    if models::Book::insert(book, &conn) {
-        println!("success");
-    } else {
-        println!("failed");
-    }
+fn main() {
+    rocket().launch();
 }
